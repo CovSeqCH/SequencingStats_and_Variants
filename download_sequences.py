d3ea1a1b1256938ebcc1dbf762b9551cbc48c75a
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-06-21 15:55:08 +0200

    Add table of sequences per unit of time

--- download_sequences.py
+++ download_sequences.py
@@ -8,33 +8,44 @@ params = {'country': 'Switzerland',
 r = requests.get(url, params)
 r.status_code
 # %%
-len(r.content)
-# %%
 df = pd.read_json(r.content)
-# %%
 # Unroll the count compression, one row per sample
 df = df.loc[df.index.repeat(df['count'])]
-# %%
 # Todo: Surveillance vs Non-Surveillance
-df.plot('date',)
+seq = df
+seq = seq.set_index('date')
+seq.head()
 # %%
-# Plots
-# 1. Sequencing over time
-# 2. Lineages over time
-# 3.
-# Need to create the views, resample by time or aggregate in some other way
+# Sequences per calendar week (week beginning and including Monday)
+seq_count_by_week = pd.DataFrame(seq.resample(
+    'W-MON', label='left', closed='left').country.count())
+seq_count_by_week.rename(columns={'country': 'sequences'}, inplace=True)
+seq_count_by_week.index.rename('start_of_cw', inplace=True)
+seq_count_by_week = seq_count_by_week.assign(
+    calendar_week=lambda x: x.index.weekofyear)
+seq_count_by_week
+# %%
+# Sequences per calendar month (labelled by last day of month)
+seq_count_by_week = pd.DataFrame(seq.resample('M').country.count())
+seq_count_by_week.rename(columns={'country': 'sequences'}, inplace=True)
+seq_count_by_week.index.rename('end_of_month', inplace=True)
+seq_count_by_week = seq_count_by_week.assign(month=lambda x: x.index.month)
+seq_count_by_week
+# %%
+# # %%
+# # %%
+# # Plots
+# # 1. Table of sequences by calendar week
 
-df.describe()
+# # %%
+# df.columns
+# # %%
+# df.head()
 
-# %%
-df.columns
-# %%
-df.head()
-
-# %%
-df.pangolinLineage.value_counts()[0:60]
-# %%
-df.dtypes
-# %%
-df.pangolinLineage.value_counts()['AY.1']
-# %%
+# # %%
+# df.pangolinLineage.value_counts()[0:60]
+# # %%
+# df.dtypes
+# # %%
+# df.pangolinLineage.value_counts()['AY.1']
+# # %%
