c527f8686f729da6368eca6608904e1b01a7fc0b
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-06-22 20:20:24 +0200

    Add sample plots to folder

--- scripts/plots.py
+++ scripts/plots.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 # %%
-df = pd.read_csv('cases_seq_by_cw_region.csv', parse_dates=['date'])
+df = pd.read_csv('data/cases_seq_by_cw_region.csv', parse_dates=['date'])
 df.set_index('date', inplace=True)
 # %%
 gen = df[df.region == 0].iloc[-20:]
@@ -13,6 +13,7 @@ ax.set_ylim(0,)
 ax.set_title("Proportion of sequenced cases by week in Switzerland")
 ax.set_ylabel("Proportion of sequenced cases")
 fig.autofmt_xdate()
+fig.savefig('plots/sequence_share_CH.pdf')
 # %%
 gen = df[df.region == 0].iloc[-20:]
 fig, ax = plt.subplots()
@@ -22,6 +23,7 @@ ax.set_ylim(0,)
 ax.set_title("Sequenced cases by week in Switzerland")
 ax.set_ylabel("Sequenced cases")
 fig.autofmt_xdate()
+fig.savefig('plots/sequences_CH.pdf')
 # %%
 fig, ax = plt.subplots()
 for i in range(1, 7):
@@ -33,6 +35,7 @@ ax.set_ylabel("Proportion of sequenced cases")
 ax.set_ylim(0,)
 ax.legend()
 fig.autofmt_xdate()
+fig.savefig('plots/sequence_share_regions.pdf')
 # %%
 gen = df[df.region == 0].iloc[-10:]
 fig, ax = plt.subplots()
@@ -40,3 +43,4 @@ ax.bar(gen.index, gen.delta/gen.sequences)
 ax.set_title("Share of Delta of sequenced cases by week in Switzerland")
 ax.set_ylabel("Share of Delta")
 fig.autofmt_xdate()
+fig.savefig('plots/delta_share_CH.pdf')
