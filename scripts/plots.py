236863e0c6e672a940b309aa67e1c1bfdaff9c22
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-06-23 18:49:04 +0200

    Improved plots

--- scripts/plots.py
+++ scripts/plots.py
@@ -1,9 +1,8 @@
-# %%
-from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import os
-# %%
+from .variant_to_pango import variant_to_lineage
 
 
 def load_data():
@@ -25,43 +24,54 @@ def save_fig(fig, output_dir, filename):
 
 
 def generate_plots(df, output_dir):
-    gen = df[df.region == 0]
+    data = df[df.region == 0]
     fig, ax = plt.subplots()
-    ax.plot(gen.index, gen.sequences/gen.cases)
+    ax.plot(data.index, data.sequences/data.cases)
     ax.axhline(0.1, ls=':')
     ax.set_ylim(0,)
     ax.set_title("Proportion of sequenced cases by week in Switzerland")
     ax.set_ylabel("Proportion of sequenced cases")
+    ax.set_xlabel("First day of calendar week")
+    ax.set_xticks(data.index)
     fig.autofmt_xdate()
     save_fig(fig, output_dir, 'sequence_share_CH.pdf')
-    # %%
-    gen = df[df.region == 0]
+
+    data = df[df.region == 0]
     fig, ax = plt.subplots()
-    ax.plot(gen.index, gen.sequences)
+    ax.plot(data.index, data.sequences)
     ax.axhline(1000, ls=':')
     ax.set_ylim(0,)
     ax.set_title("Sequenced cases by week in Switzerland")
     ax.set_ylabel("Sequenced cases")
+    ax.set_xlabel("First day of calendar week")
+    ax.set_xticks(data.index)
     fig.autofmt_xdate()
     save_fig(fig, output_dir, 'sequences_CH.pdf')
-    # %%
+
     fig, ax = plt.subplots()
     for i in range(1, 7):
-        gen = df[df.region == i]
-        ax.plot(gen.index, gen.sequences/gen.cases, label=i)
+        data = df[df.region == i]
+        ax.plot(data.index, data.sequences/data.cases, label=i)
     ax.axhline(0.1, ls=':')
     ax.set_title(
         "Proportion of sequenced cases by week and surveillance region")
     ax.set_ylabel("Proportion of sequenced cases")
+    ax.set_xlabel("First day of calendar week")
     ax.set_ylim(0,)
     ax.legend()
+    ax.set_xticks(data.index)
     fig.autofmt_xdate()
     save_fig(fig, output_dir, 'sequence_share_regions.pdf')
-    # %%
-    gen = df[df.region == 0]
+
+    data = df[df.region == 0]
     fig, ax = plt.subplots()
-    ax.bar(gen.index, gen.delta/gen.sequences)
-    ax.set_title("Share of Delta of sequenced cases by week in Switzerland")
-    ax.set_ylabel("Share of Delta")
+    for key in variant_to_lineage.keys():
+        ax.plot(data.index, data[key]/data.sequences, label=key)
+    ax.set_title("Share of variants by week in Switzerland")
+    ax.set_ylabel("Share of all sequences")
+    ax.set_xlabel("First day of calendar week")
+    ax.set_ylim(0,)
+    ax.legend()
+    ax.set_xticks(data.index)
     fig.autofmt_xdate()
-    save_fig(fig, output_dir, 'share_CH.pdf')
+    save_fig(fig, output_dir, 'variant_share_CH.pdf')
