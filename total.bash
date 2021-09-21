ba6ea4b89e5fc8181c443f7d8c3d9dfd43d3a2ab
Cornelius Roemer <cornelius.roemer@gmail.com>
2021-09-22 01:08:10 +0200

    feat: add sum to program.txt output, improve format

--- total.bash
+++ total.bash
@@ -4,9 +4,14 @@ set -x
 # Download and wrangle data
 rm temp.txt
 # Create plots and tables for March to May
+month=3
 for filename in tables/*/*
 do
-  sed -n '2p' $filename >> temp.txt
+  sed -n "2 s/.*/$month,&/p" $filename >> temp.txt
+  month=$((month + 1))
 done
 rm program.txt
-awk -F',' '{print $8}' temp.txt > program.txt
+printf "month\tsequences\n" >program.txt
+awk -F',' '{print $1 "\t" $9}' temp.txt >> program.txt
+rm temp.txt
+printf "\nsum\t$(cut -f 2 program.txt | paste -sd+ - | bc)" >>program.txt
