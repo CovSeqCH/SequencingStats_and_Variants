aec5f360e69e6bcc66336923013523cbe89c6178
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-05-05 17:58:49 +0200

    feat: add BA.2.12.1/BA.4/BA.5

--- scripts/colors.py
+++ scripts/colors.py
@@ -2,5 +2,8 @@ variant_to_color = {
     "Delta": "#66a366", 
     "BA.1*": "#A366A3",
     "BA.2*": "#cfafcf",
+    "BA.2.12.1*": "#0400e0",
+    "BA.4*": "#8a66ff",
+    "BA.5*": "#585eff",
     "others": "#216dc7",
 }
